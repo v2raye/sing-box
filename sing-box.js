13c4c8112a547617ef2122edcc9bdfbeb5378186
v2raye <61616839+v2raye@users.noreply.github.com>
2024-05-27 17:21:54 +0800

    Update sing-box.js

--- sing-box.js
+++ sing-box.js
@@ -4,7 +4,6 @@ const compatible_outbound = { tag: 'COMPATIBLE', type: 'direct' };
 let compatible = false;
 let config = JSON.parse($files[0]);
 
-// 生成代理列表
 let proxies = await produceArtifact({
   name,
   type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
@@ -12,10 +11,8 @@ let proxies = await produceArtifact({
   produceType: 'internal',
 });
 
-// 将生成的代理添加到配置的outbounds中
 config.outbounds.push(...proxies);
 
-// 定义标签与正则表达式的映射关系
 const tagRegexMap = {
   'Hong Kong': /港|hk|hongkong|kong kong|🇭🇰/i,
   'Taiwan': /台|tw|taiwan|🇹🇼/i,
@@ -25,7 +22,6 @@ const tagRegexMap = {
   'Europe': /德国|法国|英国|荷兰|de|fr|gb|nl|germany|france|Great Britain|nederland|🇩🇪|🇫🇷|🇬🇧|🇳🇱/i
 };
 
-// 为每个outbound匹配相应的代理
 config.outbounds.forEach(outbound => {
   const regex = tagRegexMap[outbound.tag];
   if (regex) {
@@ -34,7 +30,6 @@ config.outbounds.forEach(outbound => {
   }
 });
 
-// 确保每个outbound至少有一个兼容的outbound
 config.outbounds.forEach(outbound => {
   if (Array.isArray(outbound.outbounds) && outbound.outbounds.length === 0) {
     if (!compatible) {
@@ -45,12 +40,11 @@ config.outbounds.forEach(outbound => {
   }
 });
 
-// 序列化配置为JSON格式
 $content = JSON.stringify(config, null, 2);
 
-// 辅助函数，根据正则表达式获取代理标签
 function getTags(proxies, regex) {
   return proxies.filter(p => regex.test(p.tag)).map(p => p.tag);
 }
+
 
 
