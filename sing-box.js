ebef01a1f536b5db814c243868e16e14976082fd
v2raye <61616839+v2raye@users.noreply.github.com>
2024-05-27 17:02:13 +0800

    Update sing-box.js

--- sing-box.js
+++ sing-box.js
@@ -1,54 +1,55 @@
-const { type, name } = $arguments
-const compatible_outbound = {
-  tag: 'COMPATIBLE',
-  type: 'direct',
-}
+const { type, name } = $arguments;
+const compatible_outbound = { tag: 'COMPATIBLE', type: 'direct' };
 
-let compatible
-let config = JSON.parse($files[0])
+let compatible = false;
+let config = JSON.parse($files[0]);
+
+// 生成代理列表
 let proxies = await produceArtifact({
   name,
   type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
   platform: 'sing-box',
   produceType: 'internal',
-})
+});
 
-config.outbounds.push(...proxies)
+// 将生成的代理添加到配置的outbounds中
+config.outbounds.push(...proxies);
 
+// 定义标签与正则表达式的映射关系
+const tagRegexMap = {
+  'Hong Kong': /港|hk|hongkong|kong kong|🇭🇰/i,
+  'Taiwan': /台|tw|taiwan|🇹🇼/i,
+  'Japan': /日本|jp|japan|🇯🇵/i,
+  'Singapore': /^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)/i,
+  'United States': /美|us|unitedstates|united states|🇺🇸/i,
+  'Europe': /德国|法国|英国|荷兰|de|fr|gb|nl|germany|france|Great Britain|nederland|🇩🇪|🇫🇷|🇬🇧|🇳🇱/i
+};
 
-config.outbounds.map(i => {
-  if (['Hong Kong'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /港|hk|hongkong|kong kong|🇭🇰/i))
+// 为每个outbound匹配相应的代理
+config.outbounds.forEach(outbound => {
+  const regex = tagRegexMap[outbound.tag];
+  if (regex) {
+    outbound.outbounds = outbound.outbounds || [];
+    outbound.outbounds.push(...getTags(proxies, regex));
   }
-  if (['Taiwan'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /台|tw|taiwan|🇹🇼/i))
-  }
-  if (['Japan'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /日本|jp|japan|🇯🇵/i))
-  }
-  if (['Singapore'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)/i))
-  }
-  if (['United States'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /美|us|unitedstates|united states|🇺🇸/i))
-  }
-  if (['Europe'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /德国|法国|英国|荷兰|de|fr|gb|nl|germany|france|Great Britain|nederland|🇩🇪|🇫🇷|🇬🇧|🇳🇱/i))
-  }  
-})
+});
 
+// 确保每个outbound至少有一个兼容的outbound
 config.outbounds.forEach(outbound => {
   if (Array.isArray(outbound.outbounds) && outbound.outbounds.length === 0) {
     if (!compatible) {
-      config.outbounds.push(compatible_outbound)
-      compatible = true
+      config.outbounds.push(compatible_outbound);
+      compatible = true;
     }
     outbound.outbounds.push(compatible_outbound.tag);
   }
 });
 
-$content = JSON.stringify(config, null, 2)
+// 序列化配置为JSON格式
+$content = JSON.stringify(config, null, 2);
 
+// 辅助函数，根据正则表达式获取代理标签
 function getTags(proxies, regex) {
-  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+  return proxies.filter(p => regex.test(p.tag)).map(p => p.tag);
 }
+
